a6a939a7ff1c4af441af503cb6822bfd97f33421
Vadim Simakin <simmvad@gmail.com>
2021-12-23 16:05:51 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from flask import *
 import pygame as pg
+import sys
 app = Flask(__name__)
 app.secret_key = os.urandom(128)
 players = {
@@ -42,12 +43,14 @@ PROGRAM3
 used = []
 state = 0
 music = pg.mixer.Sound("bg.wav")
+alert = pg.mixer.Sound("alert.wav")
 
 
 @app.route("/admpass.html")
 def redir_admin():
-    psw = session.args.get("psw")
+    psw = request.args.get("psw")
     if psw == "KarimChik":
+        session["adm"] = "True"
         return redirect("/")
     else:
         return render_template("nosuch.html")
@@ -67,10 +70,10 @@ def admin_check():
 # Корень сайта
 @app.route("/")
 def index():
-    if session["adm"] == "True":
+    if "adm" in session and session["adm"] == "True":
         return render_template("admin-ui.html")
     if "username" not in session or session["username"] not in players:
-        return render_template("login.html", **state)
+        return render_template("login.html")
     return render_template("game.html", username=session["username"])
 
 
@@ -91,35 +94,42 @@ def login():
 def do_action(act):
     global state
     if act == "check":
-        return api_check()
+        code = request.args.get("code")
+        uname = request.args.get("uname")
+        if code == "BESTCODE":
+            players[uname] += 60
+            return "YES"
+        elif code in used:
+            return "NON"
+        elif code in codes:
+            used.append(code)
+            players[uname] += 7
+            return "YES"
+        else:
+            return "NON"
     if act == "start":
         music.play(-1)
         state = 1
     if act == "stop":
         music.stop()
         state = 0
+    if act == "coins":
+        uname = request.args.get("uname")
+        return str(players[uname])
     if act == "list":
-        res = ""
+        if len(used) == len(codes):
+            res = "Коды закончились<br>"
+        else:
+            res = ""
         for i in players:
             res += i
             res += ": "
             res += str(players[i])
             res += "<br>"
         return res
+    if act == "alert":
+        alert.play()
     return "OK"
 
-
-def api_check():
-    code = request.args.get("code")
-    uname = request.args.get("uname")
-    if code == "BESTCODE":
-        players[uname] += 60
-        return "YES"
-    elif code in used:
-        return "NON"
-    elif code in codes:
-        used.append(code)
-        players[uname] += sum(os.urandom(24)) % 20 + 1
-        return "YES"
 
 app.run(host="0.0.0.0", port=8000)
